6ea41a7071e3e50c6a011a67b854da391da83a94
VanThangg <thang.nv2882@gmail.com>
2022-04-11 09:33:59 +0700

    add

---
+++ Bai_1/IT6012.5_NguyenVanThang_2020600875.1.1.sql
@@ -0,0 +1,76 @@
+﻿create database qlnv
+on primary (
+	name = 'qlnv_dat',
+	filename='D:\Fighting\Ky_4\3. HeQT-CSDL\Bai_1\qlnv1.1.mdf',
+	size=10mb,
+	maxsize=50mb,
+	filegrowth=10mb
+)
+log on (
+	name = 'qlnv_datt',
+	filename='D:\Fighting\Ky_4\3. HeQT-CSDL\Bai_1\qlnv1.1.ldf',
+	size=1mb,
+	maxsize=5mb,
+	filegrowth=20%
+)
+go
+use qlnv
+go
+
+create table sv(
+	MaSV nvarchar(15) primary key,
+	TenSV nvarchar(30), 
+	Que nvarchar(30)
+)
+create table mon(
+	MaMH nvarchar(15) primary key,
+	TenMH nvarchar(30),
+	Sotc int default 3,
+	constraint chk_sotc check(Sotc >= 2 and Sotc <= 5),
+	constraint unique_tenmh unique(TenMH)
+)
+create table kq(
+	MaSV nvarchar(15),
+	MaMH nvarchar(15),
+	Diem float,
+	constraint chk_diem check(diem >=0 and diem <=10),
+	constraint pk primary key(MaSV, MaMH),
+	constraint fk_kq_sv foreign key(MaSV)
+	references sv(MaSV),
+	constraint fk_kq_mon foreign key(MaMH)
+	references mon(MaMH)
+)
+
+insert into sv values
+('sv1', N'Nguyễn Văn 1', N'Hà Nội'),
+('sv2', N'Nguyễn Văn 2', N'Hà Nam'),
+('sv3', N'Nguyễn Văn 3', N'Thái Bình')
+
+insert into mon values
+('mh1', N'Hệ QTCSDL', 2),
+('mh2', N'CSDL', 3),
+('mh3', N'Cấu trúc DLGT', 4)
+
+insert into kq values
+('sv1', 'mh1', 8.8),
+('sv1', 'mh2', 9.2),
+('sv2', 'mh1', 9.6),
+('sv2', 'mh2', 7),
+('sv3', 'mh2', 8.5),
+('sv3', 'mh3', 9)
+
+
+select * from sv
+select * from mon
+select * from kq
+
+
+
+
+
+
+
+
+
+
+
---
+++ Bai_1/IT6012.5_NguyenVanThang_2020600875_1.2.sql
@@ -0,0 +1,78 @@
+﻿create database qlbh
+on primary (
+	name = 'qlbh_dat_1.2',
+	filename='D:\Fighting\Ky_4\3. HeQT-CSDL\Bai_1\qlbh1.2.mdf',
+	size=10mb,
+	maxsize=50mb,
+	filegrowth=10mb
+)
+log on (
+	name = 'qlbh_datt_1.2',
+	filename='D:\Fighting\Ky_4\3. HeQT-CSDL\Bai_1\qlbh1.2.ldf',
+	size=1mb,
+	maxsize=5mb,
+	filegrowth=20%
+)
+go
+use qlbh
+go
+
+create table CongTy(
+	MaCT nvarchar(15) primary key,
+	Tenct nvarchar(30), 
+	TrangThai nvarchar(15),
+	ThanhPho nvarchar(15)
+)
+create table SanPham(
+	MaSP nvarchar(15) primary key,
+	TenSP nvarchar(30),
+	MauSac nvarchar(15) default N'Đỏ',
+	Gia float,
+	SoLuongCo int, 
+	constraint unique_tensp unique(TenSP)
+)
+create table CungUng(
+	MaCT nvarchar(15),
+	MaSP nvarchar(15),
+	SoLuongBan int,
+	constraint chk_slban check(SoLuongBan > 0),
+	constraint pk primary key(MaCT, MaSP),
+	constraint fk_cu_ct foreign key(MaCT)
+	references CongTy(MaCT),
+	constraint fk_cu_sp foreign key(MaSP)
+	references SanPham(MaSP)
+)
+
+insert into CongTy values
+('ct1', N'Công ty 1', N'Tốt', N'Hà Nội'),
+('ct2', N'Công ty 2', N'Khá', N'Hà Nam'),
+('ct3', N'Công ty 3', N'Tốt', N'Thái Bình')
+
+insert into SanPham values
+('sp1', N'Kẹo', N'Đỏ', 123.4, 20),
+('sp2', N'Gạo', N'Xanh', 12.3, 21),
+('sp3', N'Bánh', N'Đen', 14.2, 15)
+
+insert into CungUng values
+('ct1', 'sp1', 8),
+('ct1', 'sp2', 9),
+('ct2', 'sp1', 9),
+('ct2', 'sp2', 7),
+('ct3', 'sp2', 8),
+('ct3', 'sp3', 9)
+
+
+select * from CongTy
+select * from SanPham
+select * from CungUng
+
+
+
+
+
+
+
+
+
+
+
